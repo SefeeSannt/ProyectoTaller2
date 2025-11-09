375d61aac3ab7a1e9fe03b59963156fbf6fd9ad4
NadiaRamirez2025-art <nadiarzz2019@gmail.com>
2025-11-09 19:05:55 -0300

    cambios

--- CapaPresentacion/vistas/Administrador/Reportes/frmDetalleVentas.Designer.cs
+++ CapaPresentacion/vistas/Administrador/Reportes/frmDetalleVentas.Designer.cs
@@ -35,7 +35,6 @@ namespace CapaPresentacion.Vistas.Administrador.Reportes
         {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.Agregra = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -49,6 +48,7 @@ namespace CapaPresentacion.Vistas.Administrador.Reportes
             this.lblTituloTotalVentas = new System.Windows.Forms.Label();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.pnlConsultaVentas2 = new System.Windows.Forms.Panel();
+            this.lblNombreVendedor = new System.Windows.Forms.Label();
             this.lblCodVenta = new System.Windows.Forms.Label();
             this.lblDniCliente = new System.Windows.Forms.Label();
             this.lblApellidoCliente = new System.Windows.Forms.Label();
@@ -60,10 +60,9 @@ namespace CapaPresentacion.Vistas.Administrador.Reportes
             this.dtpFechaDesde = new System.Windows.Forms.DateTimePicker();
             this.lblfechaHasta = new System.Windows.Forms.Label();
             this.lblCliente = new System.Windows.Forms.Label();
-            this.txtDniCliente = new System.Windows.Forms.TextBox();
+            this.txtNombreCliente = new System.Windows.Forms.TextBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.lblFechaDesde = new System.Windows.Forms.Label();
-            this.lblNombreVendedor = new System.Windows.Forms.Label();
             this.Agregra.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaVentas)).BeginInit();
@@ -238,6 +237,18 @@ namespace CapaPresentacion.Vistas.Administrador.Reportes
             this.pnlConsultaVentas2.Size = new System.Drawing.Size(463, 415);
             this.pnlConsultaVentas2.TabIndex = 31;
             // 
+            // lblNombreVendedor
+            // 
+            this.lblNombreVendedor.AutoSize = true;
+            this.lblNombreVendedor.BackColor = System.Drawing.Color.White;
+            this.lblNombreVendedor.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreVendedor.ForeColor = System.Drawing.Color.Black;
+            this.lblNombreVendedor.Location = new System.Drawing.Point(13, 86);
+            this.lblNombreVendedor.Name = "lblNombreVendedor";
+            this.lblNombreVendedor.Size = new System.Drawing.Size(106, 17);
+            this.lblNombreVendedor.TabIndex = 23;
+            this.lblNombreVendedor.Text = "nombre vendedor";
+            // 
             // lblCodVenta
             // 
             this.lblCodVenta.AutoSize = true;
@@ -305,14 +316,6 @@ namespace CapaPresentacion.Vistas.Administrador.Reportes
             this.dgvDetalleVenta.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvDetalleVenta.BackgroundColor = System.Drawing.Color.White;
             this.dgvDetalleVenta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Franklin Gothic Medium", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvDetalleVenta.DefaultCellStyle = dataGridViewCellStyle2;
             this.dgvDetalleVenta.GridColor = System.Drawing.Color.CadetBlue;
             this.dgvDetalleVenta.Location = new System.Drawing.Point(-1, 106);
             this.dgvDetalleVenta.Name = "dgvDetalleVenta";
@@ -329,7 +332,7 @@ namespace CapaPresentacion.Vistas.Administrador.Reportes
             this.panel2.Controls.Add(this.dtpFechaDesde);
             this.panel2.Controls.Add(this.lblfechaHasta);
             this.panel2.Controls.Add(this.lblCliente);
-            this.panel2.Controls.Add(this.txtDniCliente);
+            this.panel2.Controls.Add(this.txtNombreCliente);
             this.panel2.Controls.Add(this.btnBuscar);
             this.panel2.Controls.Add(this.lblFechaDesde);
             this.panel2.Location = new System.Drawing.Point(14, 56);
@@ -373,16 +376,16 @@ namespace CapaPresentacion.Vistas.Administrador.Reportes
             this.lblCliente.ForeColor = System.Drawing.Color.Black;
             this.lblCliente.Location = new System.Drawing.Point(4, 76);
             this.lblCliente.Name = "lblCliente";
-            this.lblCliente.Size = new System.Drawing.Size(71, 17);
+            this.lblCliente.Size = new System.Drawing.Size(95, 17);
             this.lblCliente.TabIndex = 23;
-            this.lblCliente.Text = "DNI cliente:";
+            this.lblCliente.Text = "Nombre cliente:";
             // 
-            // txtDniCliente
+            // txtNombreCliente
             // 
-            this.txtDniCliente.Location = new System.Drawing.Point(5, 92);
-            this.txtDniCliente.Name = "txtDniCliente";
-            this.txtDniCliente.Size = new System.Drawing.Size(159, 20);
-            this.txtDniCliente.TabIndex = 25;
+            this.txtNombreCliente.Location = new System.Drawing.Point(5, 92);
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.Size = new System.Drawing.Size(159, 20);
+            this.txtNombreCliente.TabIndex = 25;
             // 
             // btnBuscar
             // 
@@ -408,18 +411,6 @@ namespace CapaPresentacion.Vistas.Administrador.Reportes
             this.lblFechaDesde.Size = new System.Drawing.Size(82, 17);
             this.lblFechaDesde.TabIndex = 24;
             this.lblFechaDesde.Text = "Fecha desde:";
-            // 
-            // lblNombreVendedor
-            // 
-            this.lblNombreVendedor.AutoSize = true;
-            this.lblNombreVendedor.BackColor = System.Drawing.Color.White;
-            this.lblNombreVendedor.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombreVendedor.ForeColor = System.Drawing.Color.Black;
-            this.lblNombreVendedor.Location = new System.Drawing.Point(13, 86);
-            this.lblNombreVendedor.Name = "lblNombreVendedor";
-            this.lblNombreVendedor.Size = new System.Drawing.Size(106, 17);
-            this.lblNombreVendedor.TabIndex = 23;
-            this.lblNombreVendedor.Text = "nombre vendedor";
             // 
             // frmDetalleVentas
             // 
@@ -457,7 +448,7 @@ namespace CapaPresentacion.Vistas.Administrador.Reportes
         private Button btnBuscar;
         private DateTimePicker dtpFechaDesde;
         private Label lblFechaDesde;
-        private TextBox txtDniCliente;
+        private TextBox txtNombreCliente;
         private Label lblCliente;
         private Label lblTotalVentaFilt;
         private Label lblTituloTotalVentas;
